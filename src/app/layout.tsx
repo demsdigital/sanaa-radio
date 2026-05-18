f5fd7580e5b3c5f164677d92cd840914fab5d0f6
aldosicon-8259 <aldosi.con@gmail.com>
2026-05-18 17:54:05 +0300

    fix: ثبيت شريط العاجل

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@300;400;500;700;800;900&display=swap" rel="stylesheet" />
       </head>
-      <body>
+      <body className="pt-9">
         <Ticker />
         <Navbar />
         {children}
